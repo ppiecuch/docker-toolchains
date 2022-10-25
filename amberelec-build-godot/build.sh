1230f5f348ada1399ba1cc9d00a99d69a5f5f4b6
Pawel Piecuch <piecuch.pawel@gmail.com>
2022-10-25 09:59:07 +0200

    add support for arm64 toolchain

--- amberelec-build-godot/build.sh
+++ amberelec-build-godot/build.sh
@@ -3,12 +3,22 @@
 set -e
 
 version=2022-10-20
-toolchain=1lwb-EHXE2WG5UpydcJsakwS5EBma5Klp
+arch=$(uname -p)
+if [ $arch == "i386" ] || [ $arch == "x86_64" ]; then
+    toolchain=1lwb-EHXE2WG5UpydcJsakwS5EBma5Klp
+elif [ $arch == "i386" ] || [ $arch == "x86_64" ]; then
+    toolchain=14u9k24KfCyzmn53kgbx7fExTEBHOUeaq
+else
+    echo "*** Not supportd: $arch"
+    exit 1
+fi
 
 # toolchain:
 # ----------
 # AmberELEC-GENERIC.aarch64.zip
 #  - https://drive.google.com/file/d/1lwb-EHXE2WG5UpydcJsakwS5EBma5Klp
+# AmberELEC-GENERIC.aarch64-host.arm64.zip
+#  - https://drive.google.com/file/d/14u9k24KfCyzmn53kgbx7fExTEBHOUeaq
 
 docker build $* --build-arg version=$version --build-arg toolchain=$toolchain --build-arg token_file=$token_file -t amberelec-build-godot:$version .
 
