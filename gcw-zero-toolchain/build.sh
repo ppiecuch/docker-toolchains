82c93350ed70a28db2101d86440033c9c38fccd0
Pawel Piecuch <piecuch.pawel@gmail.com>
2023-05-01 12:22:26 +0200

    update gcw toolchain

--- gcw-zero-toolchain/build.sh
+++ gcw-zero-toolchain/build.sh
@@ -5,7 +5,7 @@ set -e
 # Original GCW0 toolchain:
 # http://www.gcw-zero.com/files/opendingux-gcw0-toolchain.2014-08-20.tar.bz2
 
-version=2021-03-10
+version=2021-10-22
 toolchain=http://od.abstraction.se/opendingux/toolchain/opendingux-gcw0-toolchain.$version.tar.xz
 
 docker build --build-arg version=$version --build-arg toolchain=$toolchain -t gcw_zero_dev:$version .
